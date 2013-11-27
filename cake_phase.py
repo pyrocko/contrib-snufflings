86913b05c42b6adf5781ce2f9ba609796b41164c
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2013-11-27 15:01:15 +0100

    Improved 'Cake Phase'
    * added 'Plot Model'
    * added 'Plot Rays'
    * fixed problems with adding of models and phases

--- cake_phase.py
+++ cake_phase.py
@@ -1,73 +1,116 @@
 from pyrocko.snuffling import Snuffling, Param, Switch, Choice
-from pyrocko.pile_viewer import Marker, EventMarker, PhaseMarker
+from pyrocko.pile_viewer import PhaseMarker
 from pyrocko import orthodrome
 
 from pyrocko import cake
-import os
+import numpy as num
 
 
 class CakePhase(Snuffling):
-    
-    
+    '''
+    <html>
+    <head>
+    <style type="text/css">
+        body { margin-left:10px };
+    </style>
+    </head>
+    <body>
+    <h1 align="center">Theoretical Phase Arrivals</h1>
+    <p>
+    This snuffling uses pyrocko's <a href="http://emolch.github.io/pyrocko/v0.3/cake_doc.html">Cake</a>
+    module to calculate seismic rays for layered earth models. </p>
+    <p>
+    <b>Parameters:</b><br />
+        <b>&middot; Global shift</b>  -  Add time onset to phases. <br />
+        <b>&middot; Add Model</b>  -  Add a model to drop down menu. <br />
+        <b>&middot; Add Phase</b>  -  Add a phase definition. (GUI reset required)<br />
+    </p>
+    <p>
+    Instructions and information on Cake's syntax of seismic rays can be found in the <a href="http://emolch.github.io/pyrocko/v0.3/cake_doc.html#cmdoption-cake--phase">Cake documentation</a>.
+    </p>
+    </body>
+    </html>
+    '''
+
+
     def setup(self):
         self.set_name('Cake Phase')
-        
+
         #self._phase_names = 'PmP ~S ~P ~P(moho)s ~P(sill-top)s ~P(sill-bottom)s Pdiff'.split()
         self._phase_names = '~P Pg Sg pP P Pdiff PKP PcP PcS PKIKP pPKIKP SSP PPS SPP PSP SP PS ~PS ~SP Pn S Sn PP PPP ScS S Sdiff SS SSS PcP SKS SKIKS'.split()
 
         for iphase, name in enumerate(self._phase_names):
-            self.add_parameter(Switch(name, 'wantphase_%i' % iphase, iphase==0)) 
-        
-        self.add_parameter(Param('Global shift', 'tshift', 0., -20., 20.))
+            self.add_parameter(Switch(name, 'wantphase_%i' % iphase, iphase==0))
 
+        self._models = cake.builtin_models()
+
+        self.model_choice = Choice('Model', 'chosen_model', 
+                'ak135-f-continental.m', self._models)
+
+        self.add_parameter(self.model_choice)
+        self.add_parameter(Param('Global shift', 'tshift', 0., -20., 20.))
+        self.add_trigger('Add Phase', self.add_phase_definition)
+        self.add_trigger('Add Model', self.add_model_to_choice)
+        self.add_trigger('Plot Model', self.plot_model)
+        self.add_trigger('Plot Rays', self.plot_rays)
         self._phases = {}
         self._model = None
 
-    def call(self):
-        '''Main work routine of the snuffling.'''
-        
-        self.cleanup()
-
-        wanted = []
-        for iphase, name in enumerate(self._phase_names):
-            if getattr(self, 'wantphase_%i' % iphase):
-                if name in self._phases:
-                    phases = self._phases[name]
-                else:
-                    if name.startswith('~'):
-                        phases = [ cake.PhaseDef(name[1:]) ]
+    def wanted_phases(self):
+        try:
+            wanted = []
+            for iphase, name in enumerate(self._phase_names):
+                if getattr(self, 'wantphase_%i' % iphase):
+                    if name in self._phases:
+                        phases = self._phases[name]
                     else:
-                        phases = cake.PhaseDef.classic(name)
+                        if name.startswith('~'):
+                            phases = [ cake.PhaseDef(name[1:]) ]
+                        else:
+                            phases = cake.PhaseDef.classic(name)
 
-                    self._phases[name] = phases
-                    for pha in phases:
-                        pha.name = name
+                        self._phases[name] = phases
+                        for pha in phases:
+                            pha.name = name
 
-                wanted.extend(phases)
-        
+                    wanted.extend(phases)
+        except (cake.UnknownClassicPhase, 
+                cake.PhaseDefParseError), e:
+            self.fail(str(e))
+
+        return wanted
+
+    def call(self, plot_rays=False):
+
+        self.cleanup()
+        wanted = self.wanted_phases()
+
         if not wanted:
             return
 
         viewer = self.get_viewer()
         pile = self.get_pile()
-        event = viewer.get_active_event()
 
         event, stations = self.get_active_event_and_stations()
         
         if not stations:
             self.fail('No station information available.')
-        
-        if not self._model:
-            self._model = cake.load_model()
 
+        self.update_model()
+        model = self._model[1]
+
+        depth = event.depth
+        if depth is None:
+            depth = 0.0
+
+        allrays = []
+        alldists = []
         for station in stations:
             dist = orthodrome.distance_accurate50m(event, station)
-            depth = event.depth
-            if depth is None:
-                depth = 0.0
-            
-            rays = self._model.arrivals(phases=wanted, distances=[dist*cake.m2d], zstart=depth)
-            
+            alldists.append(dist)
+
+            rays = model.arrivals(phases=wanted, distances=[dist*cake.m2d], zstart=depth)
+
             for ray in rays:
                 time = ray.t
                 name = ray.given_phase().name
@@ -77,6 +120,68 @@ class CakePhase(Snuffling):
                 time += event.time + self.tshift
                 m = PhaseMarker([ (station.network, station.station, '*', '*') ], time, time, 2, phasename=name, event=event, incidence_angle=incidence_angle, takeoff_angle=takeoff_angle)
                 self.add_marker(m)
+
+            allrays.extend(rays)
+
+        if plot_rays:
+            fig = self.figure(name='Ray Paths')
+            from pyrocko import cake_plot
+            cake_plot.my_rays_plot(model, None, allrays, depth, 0.0, 
+                    num.array(alldists)*cake.m2d, axes=fig.gca())
+            
+            fig.canvas.draw()
+
+
+    def update_model(self):
+        if not self._model or self._model[0] != self.chosen_model:
+            self._model = (self.chosen_model, 
+                           cake.load_model(self.chosen_model))
+
+    def update_model_choices(self):
+        self.set_parameter_choices('chosen_model', self._models)
+
+    def add_model_to_choice(self):
+        '''Called from trigger 'Add Model'.
+
+        Adds another choice to the drop down 'Model' menu.
+        '''
+
+        in_model = self.input_filename('Load Model')
+        if in_model not in self._models:
+            self._models.append(in_model)
+            self.update_model_choices()
+            
+        self.set_parameter('chosen_model', in_model)
+        self.call()
+
+    def add_phase_definition(self):
+        ''' Called from trigger 'Add Phase Definition'.
+
+        Adds another phase option.
+        Requires a reset of the GUI.
+        '''
+        phase_def = str(self.input_dialog('Add New Phase' , 'Enter Phase Definition'))
+        self._phase_names.append(phase_def)
+
+        print len(self._phase_names) - 1
+        self.add_parameter(Switch(phase_def, 'wantphase_%s'%str(len(self._phase_names)-1), True))
+        self.reset_gui(reloaded=True)
+        self.call()
+
+
+    def plot_model(self):
+        self.update_model()
+
+        from pyrocko import cake_plot
+        
+        fig = self.figure(name='Model: %s' % self._model[0])
+
+        cake_plot.my_model_plot(self._model[1], axes=fig.gca())
+
+        fig.canvas.draw()
+
+    def plot_rays(self):
+        self.call(plot_rays=True)
 
 def __snufflings__():
     '''Returns a list of snufflings to be exported by this module.'''
