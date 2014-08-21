23962b78c21ca72cec8435f16bf354965978cfb0
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2014-08-21 10:20:09 +0200

    show google map as tab in snuffler

--- googleMapsSnuffling/snuffling.py
+++ googleMapsSnuffling/snuffling.py
@@ -8,6 +8,8 @@ from xmlMarker import *
 from PyQt4.QtCore import QUrl 
 from PyQt4.QtGui import QDesktopServices
 
+g_counter = 0
+
 def get_magnitude(event):
     if event.magnitude:
         mag = event.magnitude
@@ -17,111 +19,102 @@ def get_magnitude(event):
         mag = 0.
     return mag
 
+def convert_event_marker(marker):
+    ev = marker.get_event()
+    depth = ev.depth
+    if depth is None:
+        depth = 0.0
+
+    xmleventmarker = XMLEventMarker(eventname=ev.name,
+                            longitude=ev.lon,
+                            latitude=ev.lat,
+                            origintime=util.time_to_str(ev.time),
+                            depth=depth,
+                            magnitude=get_magnitude(ev),
+                            active=['no', 'yes'][marker._active])
+
+    return xmleventmarker
+
 class MapMaker(Snuffling):
     '''
     <h1>Create a map containing event and station locations using googlemaps.</h1>
 
-    Invokes the standard browser. Some browsers do not allow javascript to open
-    and read the xml-file containing the necessary information due to the "Some-Origin-Policy".
-    In that case you need to reset your standard browser.
-    I.e.: Firefox on Linux do:
-        xdg-settings set default-web-browser firefox.desktop
+    <p>
+    Invokes the standard browser if "Open in external browser" is selected.
+    Some browsers do not allow javascript to open and read the xml-file 
+    containing the necessary information due to the "Same-Origin-Policy".
+    In that case you need to reset your standard browser. I.e.: Firefox on
+    Linux do: <tt>xdg-settings set default-web-browser firefox.desktop</tt>
+    </p>
     '''
     def setup(self):
         self.set_name('Create Map in GoogleMaps')
-        self.add_parameter( Switch('Only Active Event', 'only_active', False))
+        self.add_parameter(Switch('Only active event', 'only_active', False))
+        self.add_parameter(Switch('Open in external browser',
+                                  'open_external', False))
+
+        self.set_live_update(False)
 
     def call(self):
         self.cleanup()
-        
-        viewer = self.get_viewer()
-        markers = viewer.get_markers()
-        while True:
-            try:
-                active_event, active_stations = \
-                        self.get_active_event_and_stations()
-                break
-            except AttributeError:
-                if self.only_active == True:
-                    self.set_parameter('only_active', False)
-                    self.fail('No active event found.')
 
-                active_stations = []
-                active_event = None
-                print 'Presumably no active event set'
-                break
+        viewer = self.get_viewer()
+
+        if self.only_active:
+            active_event, active_stations = \
+            self.get_active_event_and_stations()
+        else:
+            active_event = None
+            active_stations = viewer.stations.values()
+
 
         station_list=[]
-        stats = active_stations if self.only_active else viewer.stations.values() 
-
-        for stat in stats:
+        for stat in active_stations:
             if not util.match_nslc(viewer.blacklist, stat.nsl()):
-                xml_station_marker = XMLStationMarker(nsl = '.'.join(stat.nsl()),
-                                                longitude = stat.lon,
-                                                latitude = stat.lat,
-                                                active = 'yes')
+                xml_station_marker = XMLStationMarker(
+                    nsl='.'.join(stat.nsl()),
+                    longitude = stat.lon,
+                    latitude = stat.lat,
+                    active = 'yes')
 
             station_list.append(xml_station_marker)
+
         active_station_list = StationMarkerList(stations=station_list)
 
+        if self.only_active:
+            markers = [viewer.get_active_event_marker()]
+        else:
+            markers = [m for m in viewer.get_markers()
+                       if isinstance(m, gui_util.EventMarker)]
+
         ev_marker_list = []
-        if active_event is not None:
-
-            depth = active_event.depth
-            if depth is None:
-                depth = 0.0
-
-            xml_active_event_marker = XMLEventMarker(eventname = \
-                    active_event.name,
-                latitude=active_event.lat,
-                longitude=active_event.lon,
-                origintime=util.time_to_str(active_event.time),
-                depth=depth,
-                magnitude=get_magnitude(active_event),
-                active='yes')
-            ev_marker_list.append(xml_active_event_marker)    
-
         for m in markers:
-            if self.only_active:
-                break
-            if isinstance(m, gui_util.EventMarker):
-                ev = m.get_event()
-                try:
-                    depth = ev.depth
-                    if depth is None:
-                        depth = 0.0
-
-                    xmleventmarker = XMLEventMarker(eventname=ev.name,
-                                            longitude=ev.lon, 
-                                            latitude=ev.lat, 
-                                            origintime=util.time_to_str(ev.time), 
-                                            depth=depth,
-                                            magnitude=get_magnitude(ev),
-                                            active='no')
-                except guts.ValidationError:
-                    print 'invalid format'
-                    raise
-            else:
-                continue
-
+            xmleventmarker = convert_event_marker(m)
             ev_marker_list.append(xmleventmarker)
 
         event_list=EventMarkerList(events= ev_marker_list)
-        event_station_list = MarkerLists(station_marker_list=active_station_list,
-                                         event_marker_list=event_list)
+        event_station_list = MarkerLists(
+            station_marker_list=active_station_list,
+            event_marker_list=event_list)
+
         event_station_list.validate()
 
-        tempdir = tempfile.mkdtemp(prefix='googleMapsSnuffling-')
+        tempdir = tempfile.mkdtemp(dir=self.tempdir())
 
         for entry in ['loadxmldoc.js', 'map.html']:
-            shutil.copy(os.path.join(self.module_dir(), entry), os.path.join(\
-                    tempdir, entry))
+            shutil.copy(os.path.join(self.module_dir(), entry),
+                        os.path.join(tempdir, entry))
 
         markers_fn = os.path.join(tempdir, 'markers.xml')
         dump_xml(event_station_list, filename=markers_fn)
 
-        QDesktopServices.openUrl(QUrl('file://' + os.path.join(tempdir, \
-                'map.html')))
+        url = 'file://' + tempdir + '/map.html'
+        if self.open_external:
+            QDesktopServices.openUrl(QUrl(url))
+        else:
+            global g_counter
+            g_counter += 1
+            self.web_frame(url, name='GoogleMaps %i' % g_counter)
 
 
 def __snufflings__():
