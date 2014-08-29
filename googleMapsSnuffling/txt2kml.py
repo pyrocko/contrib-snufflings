81f6718ddf496972f5a3a961a59736b7ef7d4a0d
HerrMuellerluedenscheid <marius.kriegerowski@zmaw.de>
2014-08-29 15:12:26 +0200

    cover symbols which are not accepted in kml

--- googleMapsSnuffling/txt2kml.py
+++ googleMapsSnuffling/txt2kml.py
@@ -21,25 +21,32 @@ for l in f.readlines():
             pass
 f.close()
 
+replacements = {'&':'and'}
+
+def crop_invalid(string):
+    for k,v in replacements.items():
+        if k in string:
+            string=string.replace(k,v)
+
+    return string
+
 
 datastr = ''
 for i, bound in enumerate(sections):
     header = headers[i]
     datastr += '<Placemark>\n<name>%s</name>\n<LineString>\n<coordinates>\n'%\
-            " ".join(header.split()[1::])
+            crop_invalid(" ".join(header.split()[1::]))
 
     for segment in bound:
         print tuple(segment)+tuple([100])
-        datastr += "%s, %s, %s\n"%(tuple(segment)+tuple([100.]))
+        datastr += "%s,%s,%s\n"%(tuple(segment)+tuple([100.]))
     datastr += '</coordinates>\n</LineString>\n</Placemark>\n'
 
 
-filestr = '''
-<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://earth.google.com/kml/2.0"> 
+filestr = '''<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
 <Document>
-%s
-</Document>
+%s</Document>
 </kml>
 '''%datastr
 
