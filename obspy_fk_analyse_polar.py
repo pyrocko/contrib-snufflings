aaaf0d59a001b696f96a4ea6181b5ca562b1880c
Marius Kriegerowski <marius@gfz-potsdam.de>
2015-08-27 08:53:15 +0200

    obspy fk: temporal evolution plot, cleanup, help
    Conflicts:
    	obspy_fk_analyse_polar.py

--- obspy_fk_analyse_polar.py
+++ obspy_fk_analyse_polar.py
@@ -1,10 +1,13 @@
 from pyrocko.snuffling import Param, Snuffling, Choice
 from pyrocko import trace
+from obspy.core import UTCDateTime, stream
+from obspy.signal import array_analysis
+from obspy.imaging.cm import obspy_sequential as cmap
 
 from matplotlib.colorbar import ColorbarBase
 from matplotlib.colors import Normalize
-from matplotlib import cm
-import numpy as np
+import matplotlib.dates as mdates
+import numpy as num
 
 def p2o_trace(ptrace, station):
     '''Convert Pyrocko trace to ObsPy trace.'''
@@ -30,62 +33,70 @@ def p2o_trace(ptrace, station):
 class FK(Snuffling):
     '''
     <html>
+    <head>
+    <style type="text/css">
+        body { margin-left:10px };
+    </style>
+    </head>
     <body>
-    <h1>FK ANALYSIS</h1>
-    <h2>based in ObsPy</h2>
+    <h1 align='center'>FK ANALYSIS</h1>
+    <h2 align='center'>based in ObsPy</h2>
     <p>
     <u>Prerequisites</u><br>
-    This snuffling requires the ObsPy package which can be found at 
+    This snuffling requires the ObsPy package which can be found at
         <a href='https://github.com/obspy/obspy/wiki'> ObsPy's github page </a>
     </p>
     <p>
-    The easiest way to install this package is to do:
+    On way to install this package is to do:
         <PRE>
         git clone git://github.com/obspy/obspy.git
-        cd obspy 
-        sudo python setup.py install 
+        cd obspy
+        sudo python setup.py install
         </PRE>
     <p>
     <u>Usage</u><br>
-    
-     - Snuffler needs to be invoked with station information, as for example:
-            snuffler testdata.mseed --stations=testdata.stations<br>
 
-     - Zoom into the data until you see only data you desire to analyse.<br>
+     - Load station information at startup <br>
 
-     - Press the 'Run' button. <br>
+     - Zoom into the data until you see only data you desire to analyse or
+     use extended markers to selected time regions for analysis<br>
 
-    The slowness is given in s/km.
-    The location of the geometrical center is printed to the terminal.
+     - Press the 'Run' button <br>
+
     </p>
     <p>
-    <u>Error Handling</u><br>
-    
-    If you get an error like
-        <PRE>ValueError: shape mismatch: objects cannot be broadcast to a single shape</PRE>
-    you need to reduce the 'Length of Sliding Window [s]' by moving the slider to the left.
+    The slowness is given in s/km.
+    The assumed location of the geometrical center is printed to the terminal.
+    </p>
+    <p>
+    Further information can be gathered from
+    <a href="http://docs.obspy.org/master/tutorial/code_snippets/beamforming_fk_analysis.html">
+    ObsPy's FK tutorial</a>.
     </p>
     </body>
     </html>
-
     '''
-    
+
     def setup(self):
         self.set_name('FK Analysis')
-        self.add_parameter(Param('Slowness range[+-]','smax',0.5,0.1, 3))
-        self.add_parameter(Param('Number of slowness divisions','divisor',20,10,50))
-        self.add_parameter(Param('Number of radial sections','numberOfFraction',32,4,50))
-        self.add_parameter(Param('Length of Sliding Window [s]','window_lenth',1.,0.5,5.))
-        self.add_parameter(Choice('If sampling rates differ', 'downresample', 'resample',
-                                  ['resample', 'downsample', 'downsample to "target dt"']))
-        self.add_parameter(Param('target dt','target_dt', 0.2, 0., 10))
+        self.add_parameter(Param('Slowness range[+-]', 'smax', 0.2, 0., 1.))
+        self.add_parameter(Param(
+            'Number of slowness divisions', 'divisor', 20, 10, 50))
+        self.add_parameter(Param(
+            'Number of radial sections', 'numberOfFraction', 32, 4, 50))
+        self.add_parameter(Param(
+            'Length of Sliding Window [s]', 'window_lenth', 1., 0.5, 5.))
+        self.add_parameter(Param(
+            'Step fraction of Sliding Window [s]','win_frac', 0.05, 0., 1.))
+        self.add_parameter(Choice(
+            'If sampling rates differ', 'downresample', 'resample',
+            ['resample', 'downsample', 'downsample to "target dt"']))
+        self.add_parameter(Param('target dt', 'target_dt', 0.2, 0., 10))
 
         #self.add_parameter(Choice('Units: ','unit','[s/km]',('[s/km]','[s/deg]')))
         self.set_live_update(False)
 
     def call(self):
-        from obspy.core import UTCDateTime, stream
-        from obspy.signal import array_analysis
 
         self.cleanup()
         viewer = self.get_viewer()
@@ -96,8 +107,8 @@ class FK(Snuffling):
         traces = []
         for trs in self.chopper_selected_traces(fallback=True):
             for tr in trs:
-                tr.lowpass(4, viewer.lowpass)
-                tr.highpass(4, viewer.highpass)
+                tr.lowpass(2, viewer.lowpass)
+                tr.highpass(2, viewer.highpass)
 
             traces.extend(trs)
 
@@ -121,7 +132,9 @@ class FK(Snuffling):
         tmin = max([t.tmin for t in traces])
         tmax = min([t.tmax for t in traces])
         try:
-            obspy_traces = [ p2o_trace(tr, viewer.get_station(viewer.station_key(tr)) ) for tr in traces ]
+            obspy_traces = [p2o_trace(
+                tr, viewer.get_station(viewer.station_key(tr)))
+                            for tr in traces]
 
         except KeyError:
             self.fail('station information missing')
@@ -132,15 +145,12 @@ class FK(Snuffling):
 
         # Execute sonic
         kwargs = dict(
-            # slowness grid: X min, X max, Y min, Y max, Slow Step
-            sll_x=-self.smax, slm_x=self.smax, sll_y=-self.smax, slm_y=self.smax, sl_s=self.smax/self.divisor,
-            # sliding window properties
-            win_len=self.window_lenth, win_frac=0.1,
-            # frequency properties
+            sll_x=-self.smax, slm_x=self.smax, sll_y=-self.smax,
+            slm_y=self.smax, sl_s=self.smax/self.divisor,
+            win_len=self.window_lenth, win_frac=self.win_frac,
             frqlow=viewer.highpass, frqhigh=viewer.lowpass, prewhiten=0,
-            # restrict output
-            semb_thres=-1.0e9, vel_thres=-1.0e9, verbose=True, timestamp='mlabday',
-            stime=UTCDateTime(tmin), etime=UTCDateTime(tmax)
+            semb_thres=-1.0e9, vel_thres=-1.0e9, verbose=True,
+            timestamp='mlabday', stime=UTCDateTime(tmin), etime=UTCDateTime(tmax)
         )
 
         try:
@@ -149,30 +159,27 @@ class FK(Snuffling):
             from obspy.signal.array_analysis import sonic
             out = sonic(st, **kwargs)
 
-        cmap = cm.hot_r
-        pi = np.pi
+        pi = num.pi
 
-        # make output human readable, adjust backazimuth to values between 0 and 360
+        # make output human readable, adjust backazimuth to values between 0
+        # and 360
         t, rel_power, abs_power, baz, slow = out.T
         baz[baz < 0.0] += 360.
 
-        # choose number of fractions in plot (desirably 360 degree/N is an integer!)
+        # choose number of fractions in plot (desirably 360 degree/N is an
+        # integer!)
         N = int(self.numberOfFraction)
-        abins = np.arange(N + 1) * 360. / N
-        sbins = np.linspace(0., self.smax, N + 1)
+        abins = num.arange(N + 1) * 360. / N
+        sbins = num.linspace(0., self.smax, N + 1)
 
         # sum rel power in bins given by abins and sbins
-        hist, baz_edges, sl_edges = np.histogram2d(baz, slow,
+        hist, baz_edges, sl_edges = num.histogram2d(baz, slow,
                 bins=[abins, sbins], weights=rel_power)
 
         # transform to gradient
-        baz_edges = baz_edges / 180 * np.pi
+        baz_edges = baz_edges / 180. * pi
 
-        #try:
         fig = self.pylab(get='figure')
-        #except TypeError:
-        #    fig = plt.figure()
-        # add polar and colorbar axes
         cax = fig.add_axes([0.85, 0.2, 0.05, 0.5])
         ax = fig.add_axes([0.10, 0.1, 0.70, 0.7], polar=True)
         ax.grid(False)
@@ -182,18 +189,35 @@ class FK(Snuffling):
 
         # circle through backazimuth
         for i, row in enumerate(hist):
-            bars = ax.bar(left=(pi / 2 - (i + 1) * dw) * np.ones(N),
-                          height=dh * np.ones(N),
-                          width=dw, bottom=dh * np.arange(N),
+            bars = ax.bar(left=(pi / 2 - (i + 1) * dw) * num.ones(N),
+                          height=dh * num.ones(N),
+                          width=dw, bottom=dh * num.arange(N),
                           color=cmap(row / hist.max()))
 
         ax.set_xticks([pi / 2, 0, 3. / 2 * pi, pi])
         ax.set_xticklabels(['N', 'E', 'S', 'W'])
-
-        # set slowness limits
         ax.set_ylim(0., self.smax)
         ColorbarBase(cax, cmap=cmap,
                      norm=Normalize(vmin=hist.min(), vmax=hist.max()))
+
+        fig2 = self.pylab(get='figure')
+        labels = ['rel.power', 'abs.power', 'baz', 'slow']
+        xlocator = mdates.AutoDateLocator()
+        for i, lab in enumerate(labels):
+            ax = fig2.add_subplot(4, 1, i + 1)
+            ax.scatter(out[:, 0], out[:, i + 1], c=out[:, 1], alpha=0.6,
+                       edgecolors='none', cmap=cmap)
+            ax.set_ylabel(lab)
+            ax.set_xlim(out[0, 0], out[-1, 0])
+            ax.set_ylim(out[:, i + 1].min(), out[:, i + 1].max())
+            ax.xaxis.set_tick_params(which='both', direction='in')
+            ax.xaxis.set_major_locator(xlocator)
+            ax.xaxis.set_major_formatter(mdates.AutoDateFormatter(xlocator))
+            if i != 3:
+                ax.set_xticklabels([])
+        fig2.subplots_adjust(hspace=0.)
+        fig2.canvas.draw()
+        fig.canvas.draw()
 
         print 'Center of Array at latitude %s and longitude %s'%(center_lat, center_lon)
 
