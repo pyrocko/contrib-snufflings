d4453df49ee1fa635708402137bbefa6ee2b1453
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2016-09-06 09:55:20 +0200

    export_waveform: improved
    - user can now specify time step
    - better handling of overwriting

--- export_waveforms.py
+++ export_waveforms.py
@@ -1,6 +1,6 @@
-from pyrocko import io
+from pyrocko import io, io_common
 from pyrocko import model
-from pyrocko.snuffling import Snuffling, NoViewerSet, Choice, Switch
+from pyrocko.snuffling import Snuffling, NoViewerSet, Choice, Switch, Param
 
 
 class ExportWaveforms(Snuffling):
@@ -23,11 +23,11 @@ class ExportWaveforms(Snuffling):
     <p>
     Note that exporting to miniseed requires the network, station, location and
     channel codes to be of length 2, 5, 2 and 3, respectively. Codes exceeding
-    these lenghts will be silently truncated.\br
+    these lenghts will be silently truncated.<br />
     In order to have more control on code replacements it is recommended to use
-    the command line tool <b>jackseis<b> which is shipped with pyrocko.<br>
+    the command line tool <b>jackseis</b> which is shipped with pyrocko.<br />
     When exporting to miniseed it is possible to combine all traces into
-    one file.
+    one file by giving a filename without template placeholders.
     </p>
     </body>
     </html>
@@ -35,36 +35,38 @@ class ExportWaveforms(Snuffling):
 
     def setup(self):
         self.set_name('Export Waveforms')
-        self.add_parameter(Choice('Format', 'format', 'mseed',
-                                  ['mseed', 'text', 'sac', 'yaff']))
-        self.add_parameter(Switch('Combine', 'combine', False))
+        self.add_parameter(
+            Choice(
+                'Format', 'format', 'mseed', ['mseed', 'text', 'sac', 'yaff']))
+
+        self.add_parameter(
+            Param(
+                'Time length limit for output files', 'tinc', None,
+                0.1, 86400., low_is_none=True))
+
         self.add_parameter(Switch('Save Station Meta', 'save_stations', False))
         self.set_live_update(False)
 
     def call(self):
         self.cleanup()
-        if self.combine and not self.format=='mseed':
-            self.fail('"Combine" only possible when exporting "mseed"')
 
-        if self.combine:
-            template = 'traces_export'
+        if self.tinc is not None:
+            template = \
+                'trace_%n.%s.%l.%c_%(tmin_us)s'
         else:
-            template = 'trace_%(network)s.%(station)s.%(location)s.%(channel)s'
-        try:
+            template = 'trace_%n.%s.%l.%c'
 
-            if self.format == 'text':
-                default_output_filename = template + '.dat'
+        if self.format == 'text':
+            default_output_filename = template + '.dat'
 
-            else:
-                default_output_filename = template + '.' + self.format
+        else:
+            default_output_filename = template + '.' + self.format
 
-            out_filename = self.output_filename('Template for output files',
-                                                default_output_filename)
-        except NoViewerSet:
-            out_filename = self.out_filename
-        traces = self.chopper_selected_traces(fallback=True)
-        trs2save = []
-        for trs in traces:
+        out_filename = self.output_filename('Template for output files',
+                                            default_output_filename)
+
+        for trs in self.chopper_selected_traces(fallback=True, tinc=self.tinc):
+            trs2save = []
             for tr in trs:
                 if self.format == 'mseed':
                     if len(tr.network) > 2:
@@ -75,13 +77,17 @@ class ExportWaveforms(Snuffling):
                         tr.set_location(tr.location[:2])
                     if len(tr.channel) > 3:
                         tr.set_channel(tr.channel[:3])
-                if self.combine:
-                    trs2save.append(tr)
-                else:
-                    io.save(tr, out_filename, format=self.format)
 
-        if self.combine:
-            io.save(trs2save, out_filename, format=self.format)
+                trs2save.append(tr)
+
+            try:
+                io.save(
+                    trs2save, out_filename,
+                    format=self.format,
+                    overwrite=True)
+
+            except io_common.FileSaveError as e:
+                self.fail(str(e))
 
         if self.save_stations:
             stations = self.get_viewer().stations.values()
