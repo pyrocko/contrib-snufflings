5500ec38b705b364ad227f5a1a90939b11d53781
Marius Kriegerowski <marius@gfz-potsdam.de>
2016-10-17 15:21:45 +0200

    vtk: hypocenters and stations = balls and cones.

--- vtktest/snuffling.py
+++ vtktest/snuffling.py
@@ -1,69 +1,124 @@
-from pyrocko.snuffling import Snuffling, Param
-import sys 
-import vtk 
-from PyQt4 import QtCore, QtGui
-from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
-import numpy as num 
-from pyrocko import model
+import numpy as num
+import vtk
+from vtk.util import numpy_support
+
+from pyrocko.snuffling import Snuffling, Param, Switch
 from pyrocko import orthodrome as ortho
-import os
-from vtk_focmec import *
 
 class VtkTest(Snuffling):
+
+    ''' Create 3D interactive plots of events and stations using vtk.
+
+    This snuffling requires VTK to be installed including Python wrappers.
+    On debian using apt-get:
+    apt-get install python-vtk
+    '''
+
     def setup(self):
         self.set_name('VTK')
-        self.add_parameter(Param('Map Radius [km]', 'radius', 10, 10, 100))
+        self.add_parameter(Param('Superelevation', 'z_scale', 1., 1., 1000.))
+        self.add_parameter(Switch('Stations', 'want_stations', True))
+        self.add_parameter(Switch('Events', 'want_events', True))
+        self.actors = []
+        self.frame = None
+        self.scale_sources = 1.
+
+    def locations_to_ned(self, locations):
+        npoints = len(locations)
+        lats = num.zeros(npoints)
+        lons = num.zeros(npoints)
+        depths = num.zeros(npoints)
+
+        for i_e, e in enumerate(locations):
+            lats[i_e] = float(e.lat)
+            lons[i_e] = float(e.lon)
+            depths[i_e] = float(e.depth)
+
+        depths *= self.z_scale
+        nz = num.zeros(npoints)
+        ns, es = ortho.latlon_to_ne_numpy(nz, nz, lats, lons)
+
+        return ns, es, depths
+
+    def stations_to_vtkcone_actors(self, data):
+        actors = []
+        for i in xrange(data.GetNumberOfTuples()):
+            source = vtk.vtkConeSource()
+            source.SetCenter(*data.GetTuple3(i))
+            source.SetRadius(200)
+            source.SetHeight(200)
+            source.Update()
+            mapper = vtk.vtkPolyDataMapper()
+            mapper.SetInputConnection(source.GetOutputPort())
+
+            actor = vtk.vtkActor()
+            actor.SetMapper(mapper)
+            actors.append(actor)
+
+        return actors
+
+    def events_to_vtksphere_actors(self, data):
+        points = vtk.vtkPoints()
+        actors = []
+        npoints =None
+        for i in xrange(data.GetNumberOfTuples()):
+            source = vtk.vtkSphereSource()
+            source.SetCenter(*data.GetTuple3(i))
+            source.SetRadius(50)
+            source.Update()
+            mapper = vtk.vtkPolyDataMapper()
+            mapper.SetInputConnection(source.GetOutputPort())
+
+            actor = vtk.vtkActor()
+            actor.SetMapper(mapper)
+            actors.append(actor)
+
+        return actors
 
     def call(self):
         self.cleanup()
         viewer = self.get_viewer()
-        markers = self.get_selected_event_markers()
-        events = [m.get_event() for m in markers]
-        lats = [e.lat for e in events]
-        lons = [e.lon for e in events]
-        center_lat = num.mean(lats)
-        center_lon = num.mean(lons)
-        #tmin, tmax = self.get_selected_time_range(fallback=True)
-        #event_markers = filter(lambda x: x.tmin>=tmin and x.tmax<=tmax,
-        #                       viewer.markers)
+        if self.want_events:
+            markers = self.get_selected_event_markers()
+            events = [m.get_event() for m in markers]
 
-        #event_markers = filter(lambda x: isinstance(x, EventMarker), event_markers)
-        #if self.maxd:
-        #    event_markers = filter(lambda x: distance(self, x._event)<=self.maxd*km,
-        #                       event_markers)
+            ns, es, depths = self.locations_to_ned(events)
+            adata = num.array((depths, ns, es))
+            valrange_n = (num.min(adata[1, :]), num.max(adata[1, :]))
+            valrange_e = (num.min(adata[2, :]), num.max(adata[2, :]))
 
-        #if event_markers==[]:
-        #    self.fail('No events in selected area found')
+            adata = adata.flatten(order='F')
+            data = numpy_support.numpy_to_vtk(
+                adata, deep=True, array_type=vtk.VTK_FLOAT )
+            data.SetNumberOfComponents(3)
 
-        self.frame = QtGui.QFrame(self._panel_parent)
+            sphere_actors = self.events_to_vtksphere_actors(data)
+        else:
+            sphere_actors = []
 
-        self.vl = QtGui.QGridLayout()
-        self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
-        self.vtkWidget.setMinimumSize(900, 880)
-        self.vl.addWidget(self.vtkWidget)
+        if self.want_stations:
+            stations = self.get_stations()
 
-        self.ren = vtk.vtkRenderer()
-        self.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
-        self.iren = self.vtkWidget.GetRenderWindow().GetInteractor()
-        actors = []
-        normals_list, centers, colors = read_data(events=events, get=['p-axis'])
-        for i, normals in enumerate(normals_list):
-            kwargs = {"centers": centers, 'normals':normals, "return_pdm":True}
+            ns, es, depths = self.locations_to_ned(stations)
+            adata = num.array((depths, ns, es)).flatten(order='F')
+            data = numpy_support.numpy_to_vtk(
+                adata, deep=True, array_type=vtk.VTK_FLOAT)
+            data.SetNumberOfComponents(3)
 
-        colors = [(0,1,0)]*len(colors)
-        kwargs = {"centers": centers, 'normals':normals, "return_pdm":True}
-        actor1, apd = make_polydata_actor(**kwargs)
-        actors.append(actor1)
-        map_actor = setup_vtk_map_actor(center_lat, center_lon, self.radius * 1000.)
-        actors.append(map_actor)
-        setup_renderer(self.ren, actors, bboxpolydata=apd)
+            cone_actors = self.stations_to_vtkcone_actors(data)
+        else:
+            cone_actors = []
 
-        self.ren.ResetCamera()
+        frame = self.vtk_frame()
 
-        #self.ren.SetViewport(0.,0.,1.,1.)
-        self._panel_parent.add_tab('pressure', self.frame)
+        for actor in cone_actors:
+            frame.add_actor(actor)
 
-        self.iren.Initialize()
+        for actor in sphere_actors:
+            frame.add_actor(actor)
+        frame.renderer.SetBackground(0.1, 0.2, 0.4)
+        frame.init()
+
 
 def __snufflings__():
     return [VtkTest()]
