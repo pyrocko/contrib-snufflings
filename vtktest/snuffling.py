f4da996fa404c6a6129b704c6d4f5bc0b8c4d888
Marius Kriegerowski <marius@gfz-potsdam.de>
2016-10-17 18:49:57 +0200

    vtk: decimation, smoothing

--- vtktest/snuffling.py
+++ vtktest/snuffling.py
@@ -5,6 +5,9 @@ from vtk.util import numpy_support
 from pyrocko.snuffling import Snuffling, Param, Switch
 from pyrocko import orthodrome as ortho
 
+from grid_topo import setup_vtk_map_actor
+
+
 class VtkTest(Snuffling):
 
     ''' Create 3D interactive plots of events and stations using vtk.
@@ -15,15 +18,20 @@ class VtkTest(Snuffling):
     '''
 
     def setup(self):
-        self.set_name('VTK')
+        self.set_name('VTK 3D-Map')
         self.add_parameter(Param('Superelevation', 'z_scale', 1., 1., 1000.))
+        self.add_parameter(Param(
+            'Topographic decimation', 'z_decimation', 1, 1, 12))
         self.add_parameter(Switch('Stations', 'want_stations', True))
         self.add_parameter(Switch('Events', 'want_events', True))
+        self.add_parameter(Switch('Topography', 'want_topo', True))
+        self.add_parameter(Switch('Topography smoothing', 'smoothing', False))
         self.actors = []
+        self.topo_actor = None
         self.frame = None
-        self.scale_sources = 1.
+        self.set_live_update(False)
 
-    def locations_to_ned(self, locations):
+    def locations_to_ned(self, locations, has_elevation=False):
         npoints = len(locations)
         lats = num.zeros(npoints)
         lons = num.zeros(npoints)
@@ -32,7 +40,10 @@ class VtkTest(Snuffling):
         for i_e, e in enumerate(locations):
             lats[i_e] = float(e.lat)
             lons[i_e] = float(e.lon)
-            depths[i_e] = float(e.depth)
+            if has_elevation:
+                depths[i_e] = float(e.depth) - float(e.elevation)
+            else:
+                depths[i_e] = float(e.depth)
 
         depths *= self.z_scale
         nz = num.zeros(npoints)
@@ -40,13 +51,14 @@ class VtkTest(Snuffling):
 
         return ns, es, depths
 
-    def stations_to_vtkcone_actors(self, data):
+    def stations_to_vtkcone_actors(self, data, size=500.):
         actors = []
         for i in xrange(data.GetNumberOfTuples()):
             source = vtk.vtkConeSource()
             source.SetCenter(*data.GetTuple3(i))
-            source.SetRadius(200)
-            source.SetHeight(200)
+            source.SetRadius(size)
+            source.SetHeight(size)
+            source.SetDirection(0., 0., -1.)
             source.Update()
             mapper = vtk.vtkPolyDataMapper()
             mapper.SetInputConnection(source.GetOutputPort())
@@ -58,9 +70,7 @@ class VtkTest(Snuffling):
         return actors
 
     def events_to_vtksphere_actors(self, data):
-        points = vtk.vtkPoints()
         actors = []
-        npoints =None
         for i in xrange(data.GetNumberOfTuples()):
             source = vtk.vtkSphereSource()
             source.SetCenter(*data.GetTuple3(i))
@@ -77,49 +87,80 @@ class VtkTest(Snuffling):
 
     def call(self):
         self.cleanup()
-        viewer = self.get_viewer()
+        stations = []
+        events = []
+
         if self.want_events:
             markers = self.get_selected_event_markers()
-            events = [m.get_event() for m in markers]
+            if len(markers) != 0:
+                events = [m.get_event() for m in markers]
 
-            ns, es, depths = self.locations_to_ned(events)
-            adata = num.array((depths, ns, es))
-            valrange_n = (num.min(adata[1, :]), num.max(adata[1, :]))
-            valrange_e = (num.min(adata[2, :]), num.max(adata[2, :]))
+                ns, es, depths = self.locations_to_ned(events)
+                adata = num.array((es, ns, -depths))
+                adata = adata.flatten(order='F')
+                data = numpy_support.numpy_to_vtk(
+                    adata, deep=True, array_type=vtk.VTK_FLOAT)
+                data.SetNumberOfComponents(3)
 
-            adata = adata.flatten(order='F')
-            data = numpy_support.numpy_to_vtk(
-                adata, deep=True, array_type=vtk.VTK_FLOAT )
-            data.SetNumberOfComponents(3)
-
-            sphere_actors = self.events_to_vtksphere_actors(data)
-        else:
-            sphere_actors = []
+                sphere_actors = self.events_to_vtksphere_actors(data)
+            else:
+                sphere_actors = []
 
         if self.want_stations:
             stations = self.get_stations()
+            if len(stations) != 0:
+                ns, es, depths = self.locations_to_ned(stations,
+                                                       has_elevation=True)
+                adata = num.array((es, ns, -depths)).flatten(order='F')
+                data = numpy_support.numpy_to_vtk(
+                    adata, deep=True, array_type=vtk.VTK_FLOAT)
+                data.SetNumberOfComponents(3)
 
-            ns, es, depths = self.locations_to_ned(stations)
-            adata = num.array((depths, ns, es)).flatten(order='F')
-            data = numpy_support.numpy_to_vtk(
-                adata, deep=True, array_type=vtk.VTK_FLOAT)
-            data.SetNumberOfComponents(3)
+                cone_actors = self.stations_to_vtkcone_actors(data)
+            else:
+                cone_actors = []
+        if self.want_topo:
 
-            cone_actors = self.stations_to_vtkcone_actors(data)
-        else:
-            cone_actors = []
+            all_lats = []
+            all_lons = []
+
+            for s in stations:
+                all_lats.append(s.lat)
+                all_lons.append(s.lon)
+
+            for e in events:
+                all_lats.append(e.lat)
+                all_lons.append(e.lon)
+
+            center_lat, center_lon = ortho.geographic_midpoint(
+                num.array(all_lats), num.array(all_lons))
+
+            center_lats = num.array([center_lat]*len(all_lats))
+            center_lons = num.array([center_lon]*len(all_lons))
+            distances = ortho.distance_accurate50m_numpy(
+                num.array(all_lats), num.array(all_lons),
+                center_lats, center_lons)
+
+            topo_actor = setup_vtk_map_actor(center_lat, center_lon,
+                                             num.max(distances),
+                                             super_elevation=self.z_scale,
+                                             decimation=int(self.z_decimation),
+                                             smoothing=self.smoothing)
 
         frame = self.vtk_frame()
 
         for actor in cone_actors:
+            actor.GetProperty().SetColor(0., 0., 1.)
             frame.add_actor(actor)
 
         for actor in sphere_actors:
+            actor.GetProperty().SetColor(1., 0., 0.)
             frame.add_actor(actor)
+
+        frame.add_actor(topo_actor)
         frame.renderer.SetBackground(0.1, 0.2, 0.4)
         frame.init()
 
 
 def __snufflings__():
     return [VtkTest()]
-
