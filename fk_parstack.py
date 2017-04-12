37d48a08284bf3a47adbb098c0fdcefc44c26062
Marius Kriegerowski <marius@gfz-potsdam.de>
2017-04-12 14:57:27 +0200

    phase weighted stack, images

--- fk_parstack.py
+++ fk_parstack.py
@@ -2,7 +2,7 @@ import numpy as num
 import time
 
 from pyrocko.snuffling import Param, Snuffling, Choice, Switch
-from scipy.signal import fftconvolve, argrelextrema, lfilter
+from scipy.signal import fftconvolve, argrelextrema, lfilter, hilbert
 from pyrocko import orthodrome as ortho
 from pyrocko import parstack
 from pyrocko import util
@@ -12,7 +12,17 @@ d2r = num.pi/180.
 km = 1000.
 
 
-def get_center_from_stations(stations):
+def instantaneous_phase(signal):
+    analytic_signal = hilbert(signal)
+    return num.unwrap(num.angle(analytic_signal))
+
+
+def get_instantaneous_frequency(signal, fs):
+    inst_phase = instantaneous_phase(signal)
+    return (num.diff(inst_phase) / (2.0*num.pi) * fs)
+
+
+def get_center_station(stations, select_closest=False):
     ''' gravitations center of *stations* list.'''
     n = len(stations)
     slats = num.empty(n)
@@ -21,22 +31,28 @@ def get_center_from_stations(stations):
         slats[i] = s.lat
         slons[i] = s.lon
 
-    return ortho.geographic_midpoint(slats, slons)
+    center_lat, center_lon = ortho.geographic_midpoint(slats, slons)
+
+    if select_closest:
+        center_lats = num.ones(n)*center_lat
+        center_lons = num.ones(n)*center_lon
+        dists = ortho.distance_accurate50m_numpy(center_lats, center_lons, slats, slons)
+        return stations[num.argmin(dists)]
+    else:
+        return model.Station(center_lat, center_lon)
 
 
-def get_theoretical_backazimuth(event, stations):
-    center_lat, center_lon = get_center_from_stations(stations)
-    return (ortho.azimuth_numpy(event.lat, event.lon, center_lat, center_lon)\
+def get_theoretical_backazimuth(event, stations, center_station):
+    return (ortho.azimuth_numpy(event.lat, event.lon, center_station.lat, center_station.lon)\
             + 180.) % 360.
 
 
-def get_shifts(stations, bazis, slownesses):
+def get_shifts(stations, center_station, bazis, slownesses):
     ''' shape = (len(bazi), len(slow))'''
     lats = num.array([s.lat for s in stations])
     lons = num.array([s.lon for s in stations])
-    center_lat, center_lon = ortho.geographic_midpoint(lats, lons)
-    lat0 = num.array([center_lat] * len(stations))
-    lon0 = num.array([center_lon] * len(stations))
+    lat0 = num.array([center_station.lat] * len(stations))
+    lon0 = num.array([center_station.lon] * len(stations))
     ns, es = ortho.latlon_to_ne_numpy(lat0, lon0, lats, lons)
     station_vector = num.array((ns, es)).T
     bazis = bazis * d2r
@@ -132,7 +148,10 @@ class FK(Snuffling):
     within that figure indicates the position of the maximum. Based on that
     slowness/back-azimuth the other two coherence maps are generated which
     show the coherence in the slowness and back-azimuth domain for that
-    specific maximum of that processing block.
+    specific maximum of that processing block.<br>
+
+    Picinbono, et. al, 1997, On Instantaneous Amplitude and Phase of Signals, 552
+    IEEE TRANSACTIONS ON SIGNAL PROCESSING, 45, 3, March 1997
 
 
     </body>
@@ -159,6 +178,7 @@ class FK(Snuffling):
             'Use channels', 'want_channel', '*',
             ['*', '*Z', '*E', '*N', 'SHZ', 'BHZ', 'p0']))
         self.add_parameter(Switch('Show', 'want_all', True))
+        self.add_parameter(Switch('Phase weighted stack', 'want_pws', False))
         self.set_live_update(False)
         self.irun = 0
 
@@ -208,8 +228,11 @@ class FK(Snuffling):
 
         # make sure that only visible stations are used
         use_stations = stations
+        center_station = get_center_station(use_stations, select_closest=True)
+        print 'Center station: ', center_station
         shift_table = get_shifts(
             stations=use_stations,
+            center_station=center_station,
             bazis=bazis,
             slownesses=slownesses)
 
@@ -219,8 +242,16 @@ class FK(Snuffling):
         npad = num.max(num.abs(shifts))
         tpad = npad * deltat_cf
 
+        # additional padding for cross over fading
+        npad_fade = 30
+        tpad_fade = npad_fade * deltat_cf
+
+        npad += npad_fade
+        tpad += tpad_fade
+
         frames = None
         tinc_add = tinc_use or 0
+
         for traces in self.chopper_selected_traces(
                 tinc=tinc_use, tpad=tpad, fallback=True,
                 want_incomplete=False, trace_selector=trace_selector):
@@ -229,6 +260,7 @@ class FK(Snuffling):
                 print 'No traces matched'
                 continue
 
+            # should be correct
             t_min = traces[0].tmin + tpad
             t_max = traces[0].tmax + tpad
 
@@ -238,17 +270,18 @@ class FK(Snuffling):
 
             shift_table = get_shifts(
                 stations=use_stations,
+                center_station=center_station,
                 bazis=bazis,
                 slownesses=slownesses)
 
             shifts = num.round(shift_table / deltat_cf).astype(num.int32)
+
             wmin = traces[0].tmin
             wmax = wmin + tinc_add
 
             iwmin = int(round((wmin-wmin) / deltat_cf))
             iwmax = int(round((wmax-wmin) / deltat_cf))
             lengthout = iwmax - iwmin
-
             arrays = num.zeros((len(traces), lengthout + npad*2))
 
             # add lowpass/highpass array!!
@@ -258,23 +291,24 @@ class FK(Snuffling):
                     tr.highpass(4, viewer.highpass, demean=True)
                 if viewer.lowpass:
                     tr.lowpass(4, viewer.lowpass)
+
+                # FAKE NEWS
                 arrays[itr] = tr.get_ydata()
 
             ntraces = len(traces)
 
-            if viewer.highpass:
-                arrays = highpass_array(arrays, deltat_cf, 4, viewer.highpass)
-            if viewer.lowpass:
-                arrays = lowpass_array(arrays, deltat_cf, 4, viewer.lowpass)
-            arrays *= arrays
+            #if viewer.highpass:
+            #    arrays = highpass_array(arrays, deltat_cf, 4, viewer.highpass)
+            #if viewer.lowpass:
+            #    arrays = lowpass_array(arrays, deltat_cf, 4, viewer.lowpass)
+            #arrays *= arrays
 
             _arrays = []
             for itr, tr in enumerate(traces):
                 if taper is not None:
-                    ydata = fftconvolve(arrays[itr], taper)
+                    ydata = fftconvolve(arrays[itr], taper, mode='same')
                 else:
                     ydata = arrays[itr]
-
                 _arrays.append(num.asarray(ydata, dtype=num.float64))
             arrays = _arrays
 
@@ -294,7 +328,7 @@ class FK(Snuffling):
 
             # theoretical bazi
             if event is not None:
-                azi_theo = get_theoretical_backazimuth(event, use_stations)
+                azi_theo = get_theoretical_backazimuth(event, use_stations, center_station)
                 print('theoretical azimuth %s degrees' % (azi_theo))
 
             print('processing time: %s seconds' % (time.time()-t1))
@@ -304,21 +338,21 @@ class FK(Snuffling):
                 return
 
             frames_reshaped = frames.reshape((n_bazis, n_slow, lengthout))
-            times = num.linspace(t_min, t_max, lengthout)
+            times = num.linspace(t_min-tpad_fade, t_max+tpad_fade, lengthout)
 
             # --------------------------------------------------------------
             # Windowed maxima search
             # --------------------------------------------------------------
-            #fig1 = self.pylab(name='FK: Power (%i)'%self.irun, get='figure')
-            #power_vs_t = num.amax(num.amax(frames, axis=1), axis=0)
+            fig1 = self.pylab(name='FK: Power (%i)'%self.irun, get='figure')
+            power_vs_t = num.amax(num.amax(frames, axis=1), axis=0)
             #print power_vs_t
-            #axs = []
-            #for i in range(3):
-            #    axs.append(fig1.add_subplot(3, 1, i+1))
-            #print self.win_length
-            #print self.win_length * deltat_cf
+            axs = []
+            for i in range(2):
+                axs.append(fig1.add_subplot(2, 1, i+1))
+
             #plot_back_slow_time(
-            #    bazis=bazis, slownesses=slownesses, times=times, frames=frames_reshaped,
+                #bazis=bazis, slownesses=slownesses, times=times, frames=frames_reshaped,
+            #    bazis=bazis, slownesses=slownesses, times=times, frames=frames,
             #    axs=axs, window_length=self.win_length*deltat_cf)
             #ax.plot(power_vs_t)
             #i_best_t = num.argmax(num.argmax(frames, axis=0), axis=0)
@@ -396,7 +430,6 @@ class FK(Snuffling):
                         (num.round(bazi_max, 1), slow_max), transform=fig1.transFigure,
                         horizontalalignment='center', verticalalignment='bottom')
 
-
                 if azi_theo:
                     ax.arrow(azi_theo/180.*num.pi, num.min(slownesses), 0,
                              num.max(slownesses), alpha=0.5, width=0.015,
@@ -407,38 +440,43 @@ class FK(Snuffling):
 
                 fig1 = self.pylab(name='FK: Beam(%i)'%self.irun, get='figure')
 
-                ax_raw = fig1.add_subplot(3, 1, 1)
-                ax_shifted = fig1.add_subplot(3, 1, 2)
-                ax_beam = fig1.add_subplot(3, 1, 3)
+                nsubplots = 4
+                ax_raw = fig1.add_subplot(nsubplots, 1, 1)
+                ax_shifted = fig1.add_subplot(nsubplots, 1, 2)
+                ax_beam = fig1.add_subplot(nsubplots, 1, 3)
 
                 axkwargs = dict(alpha=0.3, linewidth=0.3, color='grey')
 
                 ybeam = num.zeros(lengthout)
+                ybeam_weighted = num.zeros(lengthout)
                 for i, (shift, array) in enumerate(zip(shifts.T, arrays)):
                     ax_raw.plot(times, array[npad: -npad], **axkwargs)
                     ishift = shift[imax_bazi_slow]
                     ax_shifted.plot(times, array[npad-ishift: -npad-ishift], **axkwargs)
-                    ybeam += traces[i].get_ydata()[npad-ishift: -npad-ishift]
 
-                ax_beam.plot(times, ybeam, color='black')
+                    ydata = traces[i].get_ydata()[npad-ishift: -npad-ishift]
+                    ybeam += ydata
+
+                    # calculate phase weighting
+                    if self.want_pws:
+                        ph_inst = instantaneous_phase(ydata)
+                        ybeam_weighted += num.abs(num.exp(ph_inst))**4
+
+                #ax_beam.plot(times, ybeam, color='black')
+                ax_beam.plot(ybeam, color='black')
                 ax_raw.set_title('Characteristic Function')
                 ax_shifted.set_title('Shifted CF')
-                ax_beam.set_title('Beam')
+                ax_beam.set_title('Linear Stack')
 
-                beam_tr = trace.Trace(tmin=t_min, ydata=ybeam, deltat=deltat_cf)
+                if self.want_pws:
+                    ax_playground = fig1.add_subplot(nsubplots, 1, 4)
+                    ax_playground.plot(ybeam*ybeam_weighted/len(arrays))
+                    ax_playground.set_title('Phase Weighted Stack')
+
+                beam_tr = trace.Trace(tmin=t_min-tpad_fade, ydata=ybeam, deltat=deltat_cf)
                 self.add_trace(beam_tr)
 
-                #power_vs_t = num.amax(num.amax(frames, axis=1), axis=0)
-                #print power_vs_t
-                #axs = []
-                #for i in range(3):
-                    #axs.append(fig1.add_subplot(3, 1, i+1))
-
                 self.irun += 1
-
-        if fig1:
-            fig1.canvas.draw()
-
 
     def adjust_polar_axis(self, ax):
         ax.set_theta_zero_location('N')
@@ -468,32 +506,31 @@ def plot_back_slow_time(bazis, slownesses, times, frames, axs, window_length):
     nslow = len(slownesses)
     ntimes = len(times)
 
-    maxvals = num.argmax(frames, axis=0)
-    print maxvals.shape
+    #maxvals = num.argmax(frames, axis=0)
+    maxvals = num.amax(frames, axis=0)
     # find maxima of *window_length* consecutive values:
     n = maxvals.size
-    print n
-    print nbazis*nslow
     i_greatest_divisor = int(window_length/n)*n
-    imax = num.argmax(num.reshape(maxvals, (i_greatest_divisor, -1)), axis=1)
+    #imax = num.argmax(num.reshape(maxvals, (i_greatest_divisor, -1)), axis=1)
 
     # evaluate local extrema:
-    # ilocal_max = argrelextrema(maxvals, num.greater)[0]
+    #ilocal_max = argrelextrema(maxvals, num.greater)[0]
 
-    # max_vs_t = num.argmax(frames[:, ilocal_max], axis=0)
+    #max_vs_t = num.argmax(frames[:, ilocal_max], axis=0)
     #ilocal_max_b, ilocal_max_s = num.unravel_index(max_vs_t, (nbazis, nslow))
 
-    ilocal_max_b, ilocal_max_s = num.unravel_index(imax, (nbazis, nslow))
+    #ilocal_max_b, ilocal_max_s = num.unravel_index(imax, (nbazis, nslow))
 
-    max_times = times[ilocal_max]
-    max_semblance = maxvals[ilocal_max]
-    max_slownesses = slownesses[ilocal_max_s]
-    max_bazimuths = bazis[ilocal_max_b]
-    axs[0].plot(times, maxvals)
-    axs[0].plot(max_times, max_semblance, 'x')
-    axs[1].scatter(max_times, max_slownesses, c=max_semblance, s=45)
-    axs[1].set_ylim((min(slownesses), max(slownesses)))
-    axs[2].scatter(max_times, max_bazimuths, c=max_semblance, s=45)
+    #max_times = times[ilocal_max]
+    #max_semblance = maxvals[ilocal_max]
+    #max_slownesses = slownesses[ilocal_max_s]
+    #max_bazimuths = bazis[ilocal_max_b]
+    #axs[0].plot(times, maxvals)
+    axs[0].plot(maxvals)
+    #axs[0].plot(max_times, max_semblance, 'x')
+    #axs[1].scatter(max_times, max_slownesses, c=max_semblance, s=45)
+    #axs[1].set_ylim((min(slownesses), max(slownesses)))
+    #axs[2].scatter(max_times, max_bazimuths, c=max_semblance, s=45)
 
 
 def __snufflings__():
