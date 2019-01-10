2c21562cfec4591b7c818de396b50f52359053d6
Marius Kriegerowski <marius.kriegerowski@gfz-potsdam.de>
2019-01-10 15:29:32 +0100

    corrsearch: improve

--- corrsearch.py
+++ corrsearch.py
@@ -1,6 +1,7 @@
 from pyrocko.gui.snuffling import Snuffling, Param, Switch, NoViewerSet, Choice
-from pyrocko.gui.pile_viewer import Marker, EventMarker
+from pyrocko.gui.pile_viewer import Marker, EventMarker, PhaseMarker
 from pyrocko import io, trace, model
+from collections import defaultdict
 
 
 class CorrsearchSnuffling(Snuffling):
@@ -24,7 +25,8 @@ class CorrsearchSnuffling(Snuffling):
         <b>&middot; Highpass[Hz]/Lowpass[Hz]</b>  -  Filter traces before cross
                 correlation<br />
         <b>&middot; Apply to full dataset</b>  -  Work on the entire data set
-                (By default, processing is limited to data shown in viewer)<br />
+                (By default, processing is limited to data shown in viewer)
+                <br />
         <b>&middot; Normalization</b>  -  Normalize traces before cross
                 correlation. <i>Gliding</i>: time varying normalization (using
                 moving sum); <i>Normal</i>: normalizes by product of energy of
@@ -43,12 +45,24 @@ class CorrsearchSnuffling(Snuffling):
         '''Customization of the snuffling.'''
 
         self.set_name('Cross Correlation Search')
-        self.add_parameter(Param('Downsample to [Hz]', 'downsample', None, 0.1, 200., high_is_none=True))
-        self.add_parameter(Param('Highpass [Hz]', 'corner_highpass', 1., 0.001, 50.))
-        self.add_parameter(Param('Lowpass [Hz]', 'corner_lowpass', 1., 0.001, 50.))
+        self.add_parameter(
+            Param('Downsample to [Hz]', 'downsample',
+            None, 0.1, 200., high_is_none=True))
+        
+        self.add_parameter(
+            Param('Highpass [Hz]', 'corner_highpass',
+            None, 0.001, 50., low_is_none=True))
+        
+        self.add_parameter(
+            Param('Lowpass [Hz]', 'corner_lowpass',
+            None, 0.001, 50., high_is_none=True))
+        
+        self.add_parameter(Param('t search', 'tsearch', 1., 0.1, 10.))
         self.add_parameter(Switch('Apply to full dataset', 'apply_to_all', False))
-        self.add_parameter(Choice('Normalization', 'normalization', 'Off', ('Off', 'Normal', 'Gliding')))
+        self.add_parameter(Choice('Normalization', 'normalization', 'Off',
+            ('Off', 'Normal', 'Gliding')))
         self.add_parameter(Param('Threshold', 'threshold', 0.5, 0., 1.))
+        self.add_parameter(Switch('Use FFT', 'use_fft', True))
         self.set_live_update(False)
 
     def call(self):
@@ -56,8 +70,10 @@ class CorrsearchSnuffling(Snuffling):
 
         self.cleanup()
 
-        period_highpass = 1./self.corner_highpass
-        tpad = period_highpass
+        if self.corner_highpass:
+            tpad = 1./self.corner_highpass
+        else:
+            tpad = 0.
 
         try:
             viewer = self.get_viewer()
@@ -79,56 +95,69 @@ class CorrsearchSnuffling(Snuffling):
 
         pile = self.get_pile()
         masters = {}
+
+        def preprocess(_tr):
+            if self.downsample:
+                _tr.downsample_to(1./self.downsample)
+
+            if self.corner_highpass:
+                _tr.highpass(4, self.corner_highpass)
+
+            if self.corner_lowpass:
+                _tr.lowpass(4, self.corner_lowpass)
+
         for tr in pile.all(tmin=master_tmin, tmax=master_tmax, tpad=tpad):
-            if self.downsample is not None:
-                tr.downsample_to(1./self.downsample)
-            tr.highpass(4, self.corner_highpass)
-            tr.lowpass(4, self.corner_lowpass)
-            tr.chop(tr.wmin, tr.wmax)
-            masters[tr.nslc_id] = tr
+            for m in markers:
+                if m.match_nslc(tr.nslc_id):
+                    preprocess(tr)
+                    tr.chop(tr.wmin, tr.wmax)
+                    masters[tr.nslc_id] = tr
+                    break
 
         if self.apply_to_all:
             tmin, tmax = pile.get_tmin()+tpad, pile.get_tmax()
         else:
             tmin, tmax = self.get_viewer().get_time_range()
 
-        tmaster = master_tmax-master_tmin
-        tinc = min(20*tmaster, max(tmaster, tmax-tmin))
+        normalization = {'Off': None, 'Normal': 'normal', 'Gliding': 'gliding'}[self.normalization]
 
-        for traces in pile.chopper(tmin=tmin, tmax=tmax, tinc=tinc, tpad=tmaster+tpad, want_incomplete=False):
-            scc = None
-            sccn = 0
+        for traces in pile.chopper(tmin=tmin, tmax=tmax, want_incomplete=True):
+            sccs = defaultdict()
+            sccn = defaultdict()
             for b in traces:
                 nslc = b.nslc_id
                 if nslc in masters:
                     a = masters[nslc]
-                    if self.downsample is not None:
-                        b.downsample_to(1./self.downsample)
-                    b.highpass(4, self.corner_highpass)
-                    tr.lowpass(4, self.corner_lowpass)
-                    normalization = {'Off': None, 'Normal': 'normal', 'Gliding': 'gliding'}[self.normalization]
-                    c = trace.correlate(a,b, mode='valid', normalization=normalization)
-                    c.shift(-c.tmin + b.tmin)
-                    c.meta = { 'tabu' : True }
+                    preprocess(b)
 
-                    if scc is None:
+                    c = trace.correlate(
+                        a, b, mode='valid',
+                        normalization=normalization,
+                        use_fft=self.use_fft)
+
+                    c.shift(-c.tmin + b.tmin)
+                    c.meta = {'tabu': True}
+
+                    scc = sccs.get(nslc, None)
+                    if not scc:
                         scc = c.copy()
+                        scc.meta = {'tabu': True}
                         scc.wmin = b.wmin
                         scc.wmax = b.wmax
-                        scc.set_codes(network='', station='Sum Cross Correlation', location='', channel='')
-                        sccn = 1
-
+                        scc.set_codes(location=scc.location+'_SUM')
+                        sccn[nslc] = 1
                     else:
-                        scc.add(c)
-                        sccn += 1
+                        sccn[nslc] += 1
 
-            if scc is not None:
-                scc.ydata /= sccn
+                    sccs[nslc] = scc
+
+            for nslc_id, scc in sccs.items():
+                scc.ydata /= sccn[nslc_id]
                 scc.chop(scc.wmin, scc.wmax)
 
                 markers = []
-                for t, a in zip(*scc.peaks(self.threshold, tsearch=2./self.corner_highpass)):
-                    m = EventMarker(model.Event(time=t, lat=0., lon=0., name='Event(%.2g)' % a))
+                for t, a in zip(*scc.peaks(self.threshold, tsearch=self.tsearch)):
+                    m = PhaseMarker(tmin=t, tmax=t, phasename='%1.3f' % a, kind=3, nslc_ids=(nslc_id,))
                     markers.append(m)
 
                 if viewer:
